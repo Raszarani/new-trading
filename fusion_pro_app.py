1992b485e017ec4ba58a32aff5126bda30f718b9
Raszarani <ryszardsitek@gmail.com>
2026-03-04 14:00:26 +0100

    Update fusion_pro_app.py

--- fusion_pro_app.py
+++ fusion_pro_app.py
@@ -483,9 +483,9 @@ if partial_tp_toggle and not t.get("partial_done", False):
                         st.session_state.balance_history.append(st.session_state.balance_pln)
                         st.rerun()
                     
-        except Exception as e: # --- KONIEC BLOKU TRY I OBSŁUGA BŁĘDU ---
-            st.warning(f"Błąd danych dla {t['symbol']}: {e}")
-            continue
+            except Exception as e: # --- KONIEC BLOKU TRY I OBSŁUGA BŁĘDU ---
+                st.warning(f"Błąd danych dla {t['symbol']}: {e}")
+                continue
 
 
 # =====================================================
